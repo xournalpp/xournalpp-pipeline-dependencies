e9fde0eb90d3da5dab162747183176aa53441557
Fabian Keßler <fabian_kessler@gmx.de>
2021-06-14 22:04:01 +0200

    cmake-3.20 / wildcard

--- gtk/package-gtk-bin.sh
+++ gtk/package-gtk-bin.sh
@@ -27,8 +27,8 @@ cd inst
 # Copy CMake
 mkdir bin
 cp ../inst.bak/bin/cmake bin/cmake
-mkdir -p share/cmake-3.17
-cp -r ../inst.bak/share/cmake-3.17 share/
+mkdir -p share
+cp -r ../inst.bak/share/cmake-3.* share/
 
 # Copy CTest
 cp ../inst.bak/bin/ctest bin/ctest
