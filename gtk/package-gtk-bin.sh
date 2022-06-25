35d5d4b504945b8a12b86dd9b348f9725866fdc7
Roland Lötscher <roland_loetscher@hotmail.com>
2022-06-25 18:51:20 +0200

    Add gtksourceview shared

--- gtk/package-gtk-bin.sh
+++ gtk/package-gtk-bin.sh
@@ -58,6 +58,7 @@ cp -r ../inst.bak/share/glib-2.0/schemas share/glib-2.0/
 cp -r ../inst.bak/share/locale share
 cp -r ../inst.bak/share/themes share
 cp -r ../inst.bak/share/icons share
+cp -r ../inst.bak/share/gtksourceview-4 share
 cp ../inst.bak/bin/gdk-pixbuf-query-loaders bin/
 cp ../inst.bak/bin/gtk-query-immodules-3.0 bin/
 
